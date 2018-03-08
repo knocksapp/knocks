c90088c5661c972f0a4c485f8c45e167c9475048
Amr <muhammad.amro.moussa@gmail.com>
2018-03-08 16:44:12 +0200

    blade views layouts

--- resources/views/layouts/nav.blade.php
+++ resources/views/layouts/nav.blade.php
@@ -1,9 +1,15 @@
 <nav style = "background: #691a40; position: fixed !important; top : 0; z-index: 100000000;" id = "knocks_nav_vue" >
     <div class="nav-wrapper">
       <div class="container">
-        <a href="#" class="brand-logo" style="color:#fff8dc;">
+        <a href="#" class="brand-logo" style="color:#fff8dc; font-size: 220% ">
           <span class = "knocks-knocks"></span>
-          Knocks | <small style = "font-size:50%">Development platform</small></a>
+          Knocks 
+          <span style="font-size: 70%" class = "hide-on-small-only">
+          	<span  class = "knocks-develop blue-text"></span> 
+          	<span class = "orange-text" style ="font-family: monospace;" >Developer</span> 
+          	<span  class = "knocks-develop blue-text"></span>
+          </span>
+      </a>
       </div>
     </div>
   </nav>
