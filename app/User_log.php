68dc792661cac83cb970ee03739e9768084eff14
Amr <muhammad.amro.moussa@gmail.com>
2018-03-15 21:19:03 +0200

    15 mar 18 (25)

--- app/User_log.php
+++ app/User_log.php
@@ -6,26 +6,39 @@ use Illuminate\Database\Eloquent\Model;
 use Jenssegers\Agent\Agent;
 class User_log extends Model
 {
-  public function addUserLog($user , $url , $ip){
+  public function autoLog($url , $ip , $method){
+    if(auth()->check()){
+      $this->addUserLog(auth()->user()->id , $url , $ip , $method);
+    }else{
+      $this->addAnanymousLog($url , $ip , $method );
+    }
+  }
+  public function addUserLog($user , $url , $ip , $method ){
     $agent = new Agent();
   	$this->user_id = $user;
   		$this->index = json_encode(array(
   		'platform'=> $agent->platform() , 
+      'platform_version' => $agent->version($agent->platform()) , 
   		'browser'=> $agent->browser() , 
+      'browser_version' => $agent->version($agent->browser()) ,
   		'device'=> $agent->device() , 
   		'url' => $url ,
       'ip' => $ip,
+      'method' => $method , 
   	));
   	$this->save();
   }
-  public function addAnanymousLog($url , $ip){
+  public function addAnanymousLog($url , $ip , $method ){
   	$agent = new Agent();
   	$this->index = json_encode(array(
   		'platform'=> $agent->platform() , 
+      'platform_version' => $agent->version($agent->platform()) , 
   		'browser'=> $agent->browser() , 
+      'browser_version' => $agent->version($agent->browser()) ,
   		'device'=> $agent->device() , 
   		'url' => $url ,
       'ip' => $ip,
+      'method' => $method , 
   	));
   	$this->save();
   }   
