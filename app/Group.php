c872480164125a26898d9185748e871486cb33c9
Amr <muhammad.amro.moussa@gmail.com>
2018-03-23 03:39:57 +0200

    23 mar 18 (31)

--- app/Group.php
+++ app/Group.php
@@ -2,185 +2,226 @@
 
 namespace App;
 
-use Illuminate\Database\Eloquent\Model;
-use App\Knock;
-use App\obj;
 use App\Group_member;
+use App\Knock;
 use App\User_request;
-class Group extends Model
-{
-  public function groupMembers(){
-    return $this->hasMany('App\Group_member' , 'group_id');
-  }
+use Illuminate\Database\Eloquent\Model;
 
-  public function memberPostion(){
-      $pos = Group_member::where('user_id','=',auth()->user()->id)
-      ->where('group_id','=',$this->id)->get()->pluck('position');
-      return $pos[0];
-  }
-  public function groupRequests(){
-      $req = User_request::where('reciver_id','=',$this->id)->where('response','=','waiting')->get()->first();
-      return count($req);
-  }
+class Group extends Model {
+	public function groupMembers() {
+		return $this->hasMany('App\Group_member', 'group_id');
+	}
 
-    public function initialize($name ,$category, $thumbnail,$preset){
-      $this->name = $name ;
-      $this->category = $category;
-      $this->thumbnail = $thumbnail;
-      $this->preset = $preset;
-      $this->index = json_encode(array(
-            "members_count" => 1 , 
-            "requests" => []
-      ));
-      $this->save();
-    }
+	public function memberPostion() {
+		$pos = Group_member::where('user_id', '=', auth()->user()->id)
+			->where('group_id', '=', $this->id)->get()->pluck('position');
+		return $pos[0];
+	}
+	public function groupRequests() {
+		$req = User_request::where('reciver_id', '=', $this->id)->where('response', '=', 'waiting')->where('parent_type', '=', 'group')->get();
+		return count($req);
+	}
 
-    public function index(){
-    	return json_decode($this->index);
-    }
+	public function initialize($name, $category, $thumbnail, $preset) {
+		$this->name = $name;
+		$this->category = $category;
+		$this->thumbnail = $thumbnail;
+		$this->preset = $preset;
+		$this->index = json_encode(array(
+			"members_count" => 1,
+			"requests" => []
+		));
+		$this->save();
+	}
 
-    public function increaseMembers(){
-    	$index = $this->index();
+	public function index() {
+		return json_decode($this->index);
+	}
 
-    	$index->members_count++;
-    	$this->index = json_encode($index);
-    	$this->update();
-    }
-    
-    public function checkUser(){
-           $mem = Group_member::where('user_id','=', auth()->user()->id )->where('group_id','=',$this->id)->get();
-           if(count($mem) > 0){
-              return true;
-           }else{
-            return false;
-           }
-    }
+	public function increaseMembers() {
+		$index = $this->index();
 
-    public function photos(){
-      $knocks = Knock::where('type' , '=' , 'group')->where('at' , '=' , $this->id)->get();
-      $res = [];
-      foreach($knocks as $knock){
-        if($knock->hasPictures()){
-          $pics = $knock->pictures();
-          for($i = 0 ; $i < count($pics); $i++)
-            array_push($res, $pics[$i]);
-        }
-      }
-      return $res;
-    }
-    public function voices(){
-      $knocks = Knock::where('type' , '=' , 'group')->where('at' , '=' , $this->id)->get();
-      $res = [];
-      foreach($knocks as $knock){
-        if($knock->hasVoices()){
-          array_push($res, $knock->voices());
-        }
-      }
-      return $res;
-    }
-       public function files(){
-      $knocks = Knock::where('type' , '=' , 'group')->where('at' , '=' , $this->id)->get();
-      $res = [];
-      foreach($knocks as $knock){
-        if($knock->hasFiles()){
-          $files = $knock->files();
-          for($i = 0 ; $i < count($files); $i++)
-            array_push($res, $files[$i]);
-        }
-      }
-      return $res;
-    }
-      public function videos(){
-      $knocks = Knock::where('type' , '=' , 'group')->where('at' , '=' , $this->id)->get();
-      $res = [];
-      foreach($knocks as $knock){
-        if($knock->hasVideos()){
-          $videos = $knock->videos();
-          for($i = 0 ; $i < count($videos); $i++)
-            array_push($res, $videos[$i]);
-        }
-      }
-      return $res;
-    }
-     public function decreaseMembers(){
-      $index = $this->index();
+		$index->members_count++;
+		$this->index = json_encode($index);
+		$this->update();
+	}
 
-      $index->members_count--;
-      $this->index = json_encode($index);
-      $this->update();
-    }
+	public function checkUser() {
+		$mem = Group_member::where('user_id', '=', auth()->user()->id)->where('group_id', '=', $this->id)->get();
+		if (count($mem) > 0) {
+			return true;
+		} else {
+			return false;
+		}
+	}
 
-     public function getGroupKnocks($limits){
+	public function photos() {
+		$knocks = Knock::where('type', '=', 'group')->where('at', '=', $this->id)->get();
+		$res = [];
+		foreach ($knocks as $knock) {
+			if ($knock->hasPictures()) {
+				$pics = $knock->pictures();
+				for ($i = 0; $i < count($pics); $i++) {
+					array_push($res, $pics[$i]);
+				}
 
-      $limits = json_decode($limits);
-      if($limits->max == null && $limits->min == null){
-           $knocks = array();
-          $current =  Knock::where('type','=','group')->where('at','=',$this->id)->get()->pluck('id');
-        foreach($current as $c ) array_push($knocks, $c);
+			}
+		}
+		return $res;
+	}
+	public function voices() {
+		$knocks = Knock::where('type', '=', 'group')->where('at', '=', $this->id)->get();
+		$res = [];
+		foreach ($knocks as $knock) {
+			if ($knock->hasVoices()) {
+				array_push($res, $knock->voices());
+			}
+		}
+		return $res;
+	}
+	public function files() {
+		$knocks = Knock::where('type', '=', 'group')->where('at', '=', $this->id)->get();
+		$res = [];
+		foreach ($knocks as $knock) {
+			if ($knock->hasFiles()) {
+				$files = $knock->files();
+				for ($i = 0; $i < count($files); $i++) {
+					array_push($res, $files[$i]);
+				}
 
-          if(count($knocks) == 0) return array ('knocks' => [] , 'last_index' => null);
-          rsort($knocks);
-          $collection = collect($knocks);
-          return array ('knocks' => $collection->chunk(3)->toArray()[0] , 'last_index' => null);
-      }elseif($limits->max != null ){
-       $knocks = array();
-          $current =  Knock::where('type','=','group')->where('id' , '>' , $limits->max)->where('at','=',$this->id)->get()->pluck('id');
-        foreach($current as $c ) array_push($knocks, $c);
-          if(count($knocks) < 3 && $limits->min){
-            $current =  $this->knocks()->where('type','=','group')->where('id' , '<' , $limits->min)->get()->pluck('id');
-           foreach($current as $c ) array_push($knocks, $c);
+			}
+		}
+		return $res;
+	}
+	public function videos() {
+		$knocks = Knock::where('type', '=', 'group')->where('at', '=', $this->id)->get();
+		$res = [];
+		foreach ($knocks as $knock) {
+			if ($knock->hasVideos()) {
+				$videos = $knock->videos();
+				for ($i = 0; $i < count($videos); $i++) {
+					array_push($res, $videos[$i]);
+				}
 
-          }
-          rsort($knocks);
-          if(count($knocks) == 0) return array ('knocks' => [] , 'last_index' => null);
-          $collection = collect($knocks);
-          if($limits->last_index == null && count($collection)  == 0)
-          return array ( 'knocks' => [] , 'last_index' => null);
-          if($limits->last_index == null && count($collection) > 0)
-          return array ( 'knocks' => $collection->chunk(3)->toArray()[0] , 'last_index' => 0 );
-          elseif($limits->last_index == count($collection)->chunk(3) - 1){
-            return array ( 'knocks' => [] , 'last_index' => count($collection)->chunk(3) - 1 );
-          }else{
-            return array( 'knocks' => $collection->chunk(3)->toArray()[$last_index] , 'last_index' => $last_index+1 );
-          }
-      }
+			}
+		}
+		return $res;
+	}
+	public function decreaseMembers() {
+		$index = $this->index();
 
-    }
+		$index->members_count--;
+		$this->index = json_encode($index);
+		$this->update();
+	}
 
-    public function getGroupKnocksRegular(){
-          $knocks = array();
-          $current =  Knock::where('type','=','group')->where('at','=',$this->id)->get()->pluck('id');
-        foreach($current as $c ) array_push($knocks, $c);
-          if(count($knocks) == 0) return array ('knocks' => [] , 'last_index' => null);
-          rsort($knocks);
-          $collection = collect($knocks);
-          return array ('knocks' => $collection->chunk(3)->toArray()[0] , 'last_index' => null);
-    }
+	public function getGroupKnocks($limits) {
 
-    public function getGroupKnocksRegularMin($min){
-        if($min == null) return array ('knocks' => array(), 'last_index' => null);
+		$limits = json_decode($limits);
+		if ($limits->max == null && $limits->min == null) {
+			$knocks = array();
+			$current = Knock::where('type', '=', 'group')->where('at', '=', $this->id)->get()->pluck('id');
+			foreach ($current as $c) {
+				array_push($knocks, $c);
+			}
 
-        $knocks = array();
-        $current =  Knock::where('type','=','group')->where('at','=',$this->id)->where('id' , '<' , $min)->get()->pluck('id');
+			if (count($knocks) == 0) {
+				return array('knocks' => [], 'last_index' => null);
+			}
 
-        foreach($current as $c ) array_push($knocks, $c);
+			rsort($knocks);
+			$collection = collect($knocks);
+			return array('knocks' => $collection->chunk(3)->toArray()[0], 'last_index' => null);
+		} elseif ($limits->max != null) {
+			$knocks = array();
+			$current = Knock::where('type', '=', 'group')->where('id', '>', $limits->max)->where('at', '=', $this->id)->get()->pluck('id');
+			foreach ($current as $c) {
+				array_push($knocks, $c);
+			}
 
-          if(count($knocks) == 0) return array ('knocks' => [] , 'last_index' => null);
-          rsort($knocks);
-          $collection = collect($knocks);
-          return array ('knocks' => $collection->chunk(3)->toArray()[0] , 'last_index' => null);
-    }
+			if (count($knocks) < 3 && $limits->min) {
+				$current = $this->knocks()->where('type', '=', 'group')->where('id', '<', $limits->min)->get()->pluck('id');
+				foreach ($current as $c) {
+					array_push($knocks, $c);
+				}
 
-    public function getGroupKnocksRegularMax($max){
-        if($max == null) return $this->getGroupKnocksRegular();
-          $current =  Knock::where('type','=','group')->where('at','=',$this->id)->where('id' , '>' , $max)->get()->pluck('id');
-          $knocks = array();
-        //$current =  $this->knocks()->get()->pluck('id');
-        foreach($current as $c ) array_push($knocks, $c);
+			}
+			rsort($knocks);
+			if (count($knocks) == 0) {
+				return array('knocks' => [], 'last_index' => null);
+			}
 
-          if(count($knocks) == 0) return array ('knocks' => [] , 'last_index' => null);
-          asort($knocks);
-          $collection = collect($knocks);
-          return array ('knocks' => $collection->chunk(3)->toArray()[0] , 'last_index' => null);
-    }
+			$collection = collect($knocks);
+			if ($limits->last_index == null && count($collection) == 0) {
+				return array('knocks' => [], 'last_index' => null);
+			}
+
+			if ($limits->last_index == null && count($collection) > 0) {
+				return array('knocks' => $collection->chunk(3)->toArray()[0], 'last_index' => 0);
+			} elseif ($limits->last_index == count($collection)->chunk(3) - 1) {
+				return array('knocks' => [], 'last_index' => count($collection)->chunk(3) - 1);
+			} else {
+				return array('knocks' => $collection->chunk(3)->toArray()[$last_index], 'last_index' => $last_index + 1);
+			}
+		}
+
+	}
+
+	public function getGroupKnocksRegular() {
+		$knocks = array();
+		$current = Knock::where('type', '=', 'group')->where('at', '=', $this->id)->get()->pluck('id');
+		foreach ($current as $c) {
+			array_push($knocks, $c);
+		}
+
+		if (count($knocks) == 0) {
+			return array('knocks' => [], 'last_index' => null);
+		}
+
+		rsort($knocks);
+		$collection = collect($knocks);
+		return array('knocks' => $collection->chunk(3)->toArray()[0], 'last_index' => null);
+	}
+
+	public function getGroupKnocksRegularMin($min) {
+		if ($min == null) {
+			return array('knocks' => array(), 'last_index' => null);
+		}
+
+		$knocks = array();
+		$current = Knock::where('type', '=', 'group')->where('at', '=', $this->id)->where('id', '<', $min)->get()->pluck('id');
+
+		foreach ($current as $c) {
+			array_push($knocks, $c);
+		}
+
+		if (count($knocks) == 0) {
+			return array('knocks' => [], 'last_index' => null);
+		}
+
+		rsort($knocks);
+		$collection = collect($knocks);
+		return array('knocks' => $collection->chunk(3)->toArray()[0], 'last_index' => null);
+	}
+
+	public function getGroupKnocksRegularMax($max) {
+		if ($max == null) {
+			return $this->getGroupKnocksRegular();
+		}
+
+		$current = Knock::where('type', '=', 'group')->where('at', '=', $this->id)->where('id', '>', $max)->get()->pluck('id');
+		$knocks = array();
+		//$current =  $this->knocks()->get()->pluck('id');
+		foreach ($current as $c) {
+			array_push($knocks, $c);
+		}
+
+		if (count($knocks) == 0) {
+			return array('knocks' => [], 'last_index' => null);
+		}
+
+		asort($knocks);
+		$collection = collect($knocks);
+		return array('knocks' => $collection->chunk(3)->toArray()[0], 'last_index' => null);
+	}
 }
