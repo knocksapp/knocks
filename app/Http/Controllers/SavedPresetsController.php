848dcb3c3c452e2f9a7820a3c5999e16e98f9440
Amr <muhammad.amro.moussa@gmail.com>
2018-03-23 02:15:29 +0200

    23 mar 18 (23)

--- app/Http/Controllers/SavedPresetsController.php
+++ app/Http/Controllers/SavedPresetsController.php
@@ -18,6 +18,47 @@ class SavedPresetsController extends Controller {
 		$preset->preset = json_encode($req->object);
 		$preset->user_id = auth()->user()->id;
 		$preset->save();
+
+		if ($req->default) {
+			auth()->user()->setDefaultPreset($preset->id);
+		}
+
+		return $preset->id;
+	}
+
+	public function delete(Request $req) {
+		$preset = Saved_presets::find($req->preset);
+		if ($preset == null) {
+			return 'invalid';
+		}
+
+		if ($preset->user_id != auth()->user()->id) {
+			return 'invalid';
+		}
+
+		$preset->delete();
+		$defaultPreset = auth()->user()->defaultPreset();
+		if ($defaultPreset == null) {
+			return array('outcome' => null);
+		}
+
+		if ($defaultPreset == $req->preset) {
+			auth()->user()->setDefaultPreset(null);
+			return array('outcome' => null);
+		}
+		return array('outcome' => $defaultPreset);
+	}
+	public function setAsDefault(Request $req) {
+		$preset = Saved_presets::find($req->preset);
+		if ($preset == null) {
+			return 'invalid';
+		}
+
+		if ($preset->user_id != auth()->user()->id) {
+			return 'invalid';
+		}
+
+		auth()->user()->setDefaultPreset($preset->id);
 		return 'done';
 	}
 	public function get(Request $req) {
