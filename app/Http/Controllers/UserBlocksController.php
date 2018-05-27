4118f273164e40705e0e712d45b8287612fd6d0f
khaledalabasiry <l.loda@ymail.com>
2018-05-27 03:38:45 +0200

    27 may 18 (2)

--- app/Http/Controllers/UserBlocksController.php
+++ app/Http/Controllers/UserBlocksController.php
@@ -3,8 +3,38 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\User;
 
 class UserBlocksController extends Controller
 {
-    //
+  public function blockUser(Request $request) {
+
+    $user = UserBlocksController::where('user_id' , '=' ,auth()->user()->id);
+      if(!$user->exists()) return 'invalid';
+    $user->initialaize(
+
+        auth()->user()->id => $request->user_id,
+        'blocked_user_id' => $request->blocked_user_id,
+
+    json_encode(array($request->index))
+    );
+    return 'done';
+  }
+
+  public function retriveBlockedUser(Request $request){
+      $user = UserBlocksController::find($request->user);
+     if(!$user) return 'invalid' ;
+     $blocks = $user->user_blocks()->get();
+     $array = [];
+     foreach($blocks as $block){
+        array_push($array, $block);
+     }
+     return $array ;
+  }
+  // public function retriveUserAddresses(Request $request){
+  //
+  //   $alls = UserAddresses::where('user_id','=',$request->user_id)->get();
+  //   return $alls->first();
+  //
+  // }
 }
--- app/user_blocks.php
+++ app/user_blocks.php
@@ -6,5 +6,39 @@ use Illuminate\Database\Eloquent\Model;
 
 class user_blocks extends Model
 {
-    //
+  public function user(){
+    return $this->belongsTo('App\User' , 'user_id');
+  }
+
+  public function blocked(){
+    return $this->belongsTo('App\user_blocks' , 'user_id');
+  }
+
+  public function initialize($bloacked_user_id) {
+    $this->bloacked_user_id = $bloacked_user_id
+    $this->user_id = auth()->user()->id;
+    $this->index = json_encode(array(
+      "blocked_user_count" => 1,
+
+    ));
+    $this->save();
+  }
+  public function index() {
+		return json_decode($this->index);
+	}
+  public function increaseBlockedUser() {
+		$index = $this->index();
+
+		$index->blocked_user_count++;
+		$this->index = json_encode($index);
+		$this->update();
+	}
+  public function decreaseBlockedUser() {
+		$index = $this->index();
+
+		$index->blocked_user_count--;
+		$this->index = json_encode($index);
+		$this->update();
+	}
+
 }
