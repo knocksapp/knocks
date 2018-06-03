6e45ce55a341a38dfa5a7b885fa6874b38f8f21c
Amr <muhammad.amro.moussa@gmail.com>
2018-06-03 13:17:32 +0200

    3 jun 18 (4)

--- resources/views/emails/verifyaccount.blade.php
+++ resources/views/emails/verifyaccount.blade.php
@@ -8,31 +8,31 @@
 	<div class="col s12">
 		<h4 class = "blue-grey-text">
 		<span class = "knocks-knocks"></span>
-		<static_message msg = "Welcome to KnocksApp"></static_message>
+		<span msg = "Welcome to KnocksApp"></span>
 		</h4>
 		<h5>
 		<ul class = "blue-grey-text uk-list uk-list-divider">
 			<li>
-				<span class = "animated rotateIn knocks-globe"></span>
-				<static_message classes = "animated rubberBand  " msg="Reimagining The Social Media!"></static_message>
-				<static_message classes = "animated rubberBand  " msg=", Express yourself using your voice or any type of multimedia."></static_message>
+
+				<span class = "animated rubberBand  " >Reimagining The Social Media!</span>
+				<span class = "animated rubberBand  " >, Express yourself using your voice or any type of multimedia."></span>
 			</li>
 			<li>
-				<span class = "animated rubberBand knocks-newspaper5 "></span>
-				<static_message classes = "animated rubberBand  " msg="Sharing your long text content is much better, Knocks Provides a features for text editing and many tamplates for poet and articles!"></static_message>
+
+				<span class = "animated rubberBand  " >Sharing your long text content is much better, Knocks Provides a features for text editing and many tamplates for poet and articles!</span>
 			</li>
 			<li>
-				<span class = "animated zoomIn knocks-atom2 "></span>
-				<static_message classes = "animated rubberBand  " msg="Categorize your people into Circles, this could make it much easier to communicate with a lot of people at the same time."></static_message>
+
+				<span class = "animated rubberBand  " >Categorize your people into Circles, this could make it much easier to communicate with a lot of people at the same time.</span>
 			</li>
 			<li>
-				<span class = "animated rotateIn knocks-locked4 "></span>
-				<static_message classes = "animated rubberBand" msg="Because your privacy matters!, You can set your own restrictions on every thing that belongs to you, Customizing your privacy setting is also easy and smart to guess what kind of restrictions you may like."></static_message>
+
+				<span class = "animated rubberBand" >Because your privacy matters!, You can set your own restrictions on every thing that belongs to you, Customizing your privacy setting is also easy and smart to guess what kind of restrictions you may like.</span>
 			</li>
 		</ul>
 		</h5>
 		<h5 class = "knocks_text_danger center">
-		<static_message msg = "You have not verify your account yet, press the button below to verify it."></static_message>
+		<span msg = "You have not verify your account yet, press the button below to verify it."></span>
 		</h5>
 	</div>
 	<div class="col s12 knocks_tinny_top_margin">
