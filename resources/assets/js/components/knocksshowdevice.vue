b29dd2774ab5b2110af3d5484bd22a5597485b3f
Amr <muhammad.amro.moussa@gmail.com>
2018-06-05 21:55:55 +0200

    5 jun 18 (2)

--- resources/assets/js/components/knocksshowdevice.vue
+++ resources/assets/js/components/knocksshowdevice.vue
@@ -7,10 +7,15 @@
   :xdata = "{ device : device }"
   @success = "handleDeviceInfo($event)"></knocksretriver>
   <!--retrivers-->
+  <!--Hiddens-->
+  <div class = "knocks_hidden">
+    <static_message msg = "Unknown" v-model = "msgs.unknown"></static_message>
+  </div>
+  <!--Hiddens-->
   <div>
     <knockscollapse
     unstatic_title
-    :title = "device"
+    :title = "handleName(device)"
     :icon = "map.devices[device] !== undefined ? map.devices[device].icon : 'knocks-display'"
     >
     <div slot = "content" v-loading = "deviceRetriever.loading" style="padding : 5px;">
@@ -18,7 +23,7 @@
         <thead>
           <tr><th colspan="2">
             <span class = "knocks-information3"></span>
-           {{device}}
+           {{handleName(device)}}
           </th>
         </tr></thead>
         <tbody>
@@ -62,7 +67,7 @@
               <ul class = 'uk-list uk-list-bullet'>
                 <li v-for = "os in deviceObject.os">
                   <span :class ="map.os[os] !== undefined ? map.os[os].icon : 'knocks-gears'"></span>
-                  <span>{{os}}</span>
+                  <span>{{handleName(os)}}</span>
                 </li>
               </ul>
             </td>
@@ -77,7 +82,7 @@
               <ul class = 'uk-list uk-list-bullet'>
                 <li v-for = "browser in deviceObject.browsers">
                   <span :class ="map.browsers[browser] !== undefined ? map.browsers[browser].icon : 'knocks-globe6'"></span>
-                  <span>{{browser}}</span>
+                  <span>{{handleName(browser)}}</span>
                 </li>
               </ul>
             </td>
@@ -125,17 +130,38 @@ export default {
     return {
     	map : {
     		devices : {
-    			Macintosh : { icon : 'knocksapp-screen'  }
+    			Macintosh : { icon : 'knocksapp-screen'  } , 
+          iPhone : { icon : 'knocks-mobile2'  } , 
+          iPad : { icon : 'knocks-tablet6'  } , 
+          iPod : { icon : 'knock-ipod'  } , 
+          WebKit : { icon : 'knocksapp-screen'  } , 
+          Samsung : { icon : 'knocks-phone12'  } , 
+          HTC : { icon : 'knocks-phone12'  } , 
+          Samsung : { icon : 'knocks-phone12'  } , 
+          Bot : { icon : 'knocks-microchip' }
+
     		},
     		os : {
-    			'OS X' : { icon : 'knocks-apple' }
+    			'OS X' : { icon : 'knocks-brand9' } , 
+          iOS : { icon : 'knocks-brand9' } , 
+          AndroidOS : { icon : 'knocks-brand9' } , 
+          Linux : { icon : 'knocks-brand9' } , 
+          Windows : { icon : 'knocks-brand9' } , 
     		},
         browsers : {
+          Chrome : { icon : 'knocks-chrome green-text'} , 
+          Firefox : { icon : 'knocks-firefox orange-text'},
+          Mozilla : { icon : 'knocks-firefox orange-text'},
           Opera : { icon : 'knocks-brand175' } , 
-          Chrome : { icon : 'knocks-chrome blue-text'} , 
-          Firefox : { icon : 'knocks-firefox orange-text'}
+          IE : { icon : 'knocks-internet-explorer blue-text'},
+          Edge : { icon : 'knocks-edge blue-text text-lighten-2'},
+          Safari : { icon : 'knocks-safari blue-text'},
+
         }
     	},
+      msgs : {
+        unknown : ''
+      },
     	deviceObject : null ,
       deviceRetriever : { loading : false }
     }
@@ -148,7 +174,10 @@ export default {
   	handleDeviceInfo(e){
       if(e.response != 'invalid')
         this.init(e.response)
-  	}
+  	},
+    handleName(name){
+      return name == 0 || name == null ? msgs.unknown : name
+    }
   }
 }
 </script>
