89ca3a2784c55ff6a9ef5e45234f06dd2c9bb4ac
Amr <muhammad.amro.moussa@gmail.com>
2018-06-21 18:29:43 +0200

    21 jun 18 (6)

--- resources/assets/js/components/knocksknock.vue
+++ resources/assets/js/components/knocksknock.vue
@@ -82,25 +82,20 @@
                       </knockspopover>
                     </span>
                     <el-dropdown-menu slot="dropdown" >
-                    <el-dropdown-item v-if="userId == knockObject.user_id">
-                    <div>
-                      <span class = "knocks-trashcan4 knocks_icon_border blue-text"></span>
-                      <static_message msg = "Edit" classes="blue-text"></static_message>
-                    </div>
-                    </el-dropdown-item>
+  
                     <el-dropdown-item v-if="userId == knockObject.user_id || (knockObject.at == userId && (knockObject.type == 'self' || knockObject.type == 'user'))">
                     <div @click = "deletePost()">
                       <span class = "knocks-pencil9 knocks_icon_border red-text"></span>
                       <static_message msg = "Delete" classes="red-text"></static_message>
                     </div>
                     </el-dropdown-item>
-                    <el-dropdown-item v-if = "ownerObject != null && !ownerObject.thatsMe" >
+            <!--         <el-dropdown-item v-if = "ownerObject != null && !ownerObject.thatsMe" >
                     <div class ="teal-text text-accent-4">
                       <span class = "knocks-star3 knocks_icon_border  teal-text text-accent-4"></span>
                       <static_message msg = "See" classes="teal-text text-accent-4"></static_message> {{ownerObject.name}}
                       <static_message msg = "first" classes="teal-text text-accent-4"></static_message>
                     </div>
-                    </el-dropdown-item>
+                    </el-dropdown-item> -->
                     <el-dropdown-item>
                     <div @click = "hidePost()">
                       <span class = "knocks-minus-circle2 knocks_icon_border orange-text text-lighten-1"></span>
@@ -113,12 +108,7 @@
                       <static_message msg = "Hide always" classes="red-text text-accent-2"></static_message>
                     </div>
                     </el-dropdown-item>
-                    <el-dropdown-item>
-                    <div>
-                      <span class = "knocks-shield4 knocks_icon_border  deep-orange-text text-accent-3"></span>
-                      <static_message msg = "Report" classes="deep-orange-text text-accent-3"></static_message>
-                    </div>
-                    </el-dropdown-item>
+    
                     </el-dropdown-menu>
                     </el-dropdown>
                   </div>
