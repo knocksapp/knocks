b44013f517dde074c30f364350394f3c0ddb52cf
Amr <muhammad.amro.moussa@gmail.com>
2018-06-22 22:45:09 +0200

    22 jun 18 (8)

--- resources/views/layouts/oldie.blade.php
+++ resources/views/layouts/oldie.blade.php
@@ -4,23 +4,25 @@
 		<title>KnocksApp | unsupported browser</title>
 		<link rel="stylesheet" type="text/css" href="{{asset('css/app.css')}}">
 	</head>
-	<body class = "blue" >
+	<body style="background-color: #2196f3" >
 		<div style="margin-top: 4%">
 			<div class="container">
-				<h1 class = "center white-text" style="font-size : 2000%">
+				<center>
+				<h1  style="font-size : 2000%; color : #fff">
 				<span class = "knocks-internet-explorer"></span>
 				</h1>
-				<h1 class = "center white-text" style="font-size: 400%">
+				<h1 class = "" style="font-size: 400%; color : #fff">
 				<span>Your Browser Is Not Supported</span>
 				</h1>
-				<h1 class = "center white-text">
-					It's suggested to use a new version of Chrome Or Firefox
+				<h1 class = "" style=" color : #fff">
+				It's suggested to use a new version of Chrome Or Firefox
 				</h1>
 				<hr class="uk-divider-icon">
-				<h3 class = "center white-text">
-					<span class = "knocks-knocks"></span> KnocksApp, INC
+				<h3  style="color : #fff">
+				<span class = "knocks-knocks"></span> KnocksApp, INC
 				</h3>
 			</div>
+			</center>
 		</div>
 	</body>
 </html>
