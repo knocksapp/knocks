42d3b3f0973c79ef55dc72c7cc8d616aaaa95e21
Amr <muhammad.amro.moussa@gmail.com>
2018-06-22 23:14:03 +0200

    22 jun 18 (10)

--- app/Http/Controllers/GroupMemberController.php
+++ app/Http/Controllers/GroupMemberController.php
@@ -2,103 +2,103 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
 use App\Group;
 use App\Group_member;
+use Illuminate\Http\Request;
 
-class GroupMemberController extends Controller
-{
-    public function getGroupMembers(Request $request){
-    	// $all = Group_member::where('group_id','=',$request->group_id)->where()->get();
-    	// return $all;
-        $groupmembers = \DB::table('users')
-            ->join('group_members', 'users.id', '=', 'group_members.user_id')
-            ->select('group_members.*')
-            ->where('group_members.group_id','=',$request->group_id)
-            ->where('users.full_name','LIKE','%'.$request->q.'%')->orwhere('users.nickname', 'like' , '%'.$request->q.'%')
-            ->get();
-            return $groupmembers;
-    }
-     public function checkUserInGroup(Request $request){
-             $ingroup = Group_member::where('group_id','=',$request->group)
-             ->where('user_id','=',$request->user)->get();
-             if(count($ingroup) > 0){
-             	return 'in';
-             }
-             else{
-             	 return 'out';
-             }
+class GroupMemberController extends Controller {
+	public function getGroupMembers(Request $request) {
+		// $all = Group_member::where('group_id','=',$request->group_id)->where()->get();
+		// return $all;
+		$groupmembers = \DB::table('search_queries')
+			->join('group_members', 'search_queries.query_id', '=', 'group_members.user_id')
+			->select('group_members.*')
+			->where('group_members.group_id', '=', $request->group_id)
+			->where('search_queries.query_type', '=', 'user')
+			->where('search_queries.keywords', 'LIKE', '%' . $request->q . '%')
+			->get();
+		return $groupmembers;
+	}
+	public function checkUserInGroup(Request $request) {
+		$ingroup = Group_member::where('group_id', '=', $request->group)
+			->where('user_id', '=', $request->user)->get();
+		if (count($ingroup) > 0) {
+			return 'in';
+		} else {
+			return 'out';
+		}
 
-    }
+	}
 
-     public function checkOwner(Request $request){
-        $mem = Group_member::where('group_id','=',$request->group_id)
-        ->where('user_id','=', $request->mem_id)
-        ->where('position','=','Owner')->get();
+	public function checkOwner(Request $request) {
+		$mem = Group_member::where('group_id', '=', $request->group_id)
+			->where('user_id', '=', $request->mem_id)
+			->where('position', '=', 'Owner')->get();
 
-        if(count($mem) > 0)
-            return 'true';
-        else
-            return 'false';
-    }
-    public function checkAdmin(Request $request){
-        $mems = Group_member::where('group_id','=',$request->group_id)
-        ->where('user_id','=', $request->mem_id)
-        ->where('position','=','Admin')->get();
+		if (count($mem) > 0) {
+			return 'true';
+		} else {
+			return 'false';
+		}
 
-        if(count($mems) > 0)
-            return 'true';
-        else
-            return 'false';
-    }
+	}
+	public function checkAdmin(Request $request) {
+		$mems = Group_member::where('group_id', '=', $request->group_id)
+			->where('user_id', '=', $request->mem_id)
+			->where('position', '=', 'Admin')->get();
 
-    public function removeMember(Request $request){
-        $group = Group::find($request->group_id);
-        if(Group_member::where('group_id' , '=' , $request->group_id)->where('user_id' , '=' , auth()->user()->id)->get()->first()->isAdmin()){
-            if(Group_member::where('group_id' , '=' , $request->group_id)->where('user_id' , '=' , $request->mem_id)->get()->first()->isAdmin()){
-                if(count(Group_member::where('group_id','=',$request->group_id)->where('position','=','Owner')->get()) < 2 )
-                    {
-                        return 'faild';
-                    }else{
-                     $mem = Group_member::where('group_id','=',$request->group_id)->where('user_id','=',$request->mem_id)->delete();
-                       $group->decreaseMembers();
-                        return 'done';
-                    }
-            }else{
-                 $mem = Group_member::where('group_id','=',$request->group_id)->where('user_id','=',$request->mem_id)->delete();
-        $group->decreaseMembers();
-        return 'done';
-            }
-       
-        }
-        else 
-        {
-           $mem = Group_member::where('group_id','=',$request->group_id)->where('user_id','=',$request->mem_id)->delete();
-        $group->decreaseMembers();
-        return 'done';
-        }
-    }
-    public function getMembersPosition(Request $request){
-        $mem = Group_member::where('group_id','=',$request->group_id)->where('user_id','=',$request->user_id)->get();
-        return $mem;
-    }
-    public function setMembersToAdmin(Request $request){
-        $upd = Group_member::where('group_id','=',$request->group_id)->where('user_id','=',$request->user_id)->where('position','=','Member')->get()->first();
-                $upd->position = 'Admin';
-                $upd->update();
-                return 'done';
-    }
-    public function setAdminToMember(Request $request){
-        $upd = Group_member::where('group_id','=',$request->group_id)->where('user_id','=',$request->user_id)->where('position','=','Admin')->get()->first();
-                $upd->position = 'Member';
-                $upd->update();
-                return 'done';
-    }
-      public function setToOwner(Request $request){
-        $upd = Group_member::where('group_id','=',$request->group_id)->where('user_id','=',$request->user_id)->get()->first();
-                $upd->position = 'Owner';
-                $upd->update();
-                return 'done';
-    }
-    
+		if (count($mems) > 0) {
+			return 'true';
+		} else {
+			return 'false';
+		}
+
+	}
+
+	public function removeMember(Request $request) {
+		$group = Group::find($request->group_id);
+		if (Group_member::where('group_id', '=', $request->group_id)->where('user_id', '=', auth()->user()->id)->get()->first()->isAdmin()) {
+			if (Group_member::where('group_id', '=', $request->group_id)->where('user_id', '=', $request->mem_id)->get()->first()->isAdmin()) {
+				if (count(Group_member::where('group_id', '=', $request->group_id)->where('position', '=', 'Owner')->get()) < 2) {
+					return 'faild';
+				} else {
+					$mem = Group_member::where('group_id', '=', $request->group_id)->where('user_id', '=', $request->mem_id)->delete();
+					$group->decreaseMembers();
+					return 'done';
+				}
+			} else {
+				$mem = Group_member::where('group_id', '=', $request->group_id)->where('user_id', '=', $request->mem_id)->delete();
+				$group->decreaseMembers();
+				return 'done';
+			}
+
+		} else {
+			$mem = Group_member::where('group_id', '=', $request->group_id)->where('user_id', '=', $request->mem_id)->delete();
+			$group->decreaseMembers();
+			return 'done';
+		}
+	}
+	public function getMembersPosition(Request $request) {
+		$mem = Group_member::where('group_id', '=', $request->group_id)->where('user_id', '=', $request->user_id)->get();
+		return $mem;
+	}
+	public function setMembersToAdmin(Request $request) {
+		$upd = Group_member::where('group_id', '=', $request->group_id)->where('user_id', '=', $request->user_id)->where('position', '=', 'Member')->get()->first();
+		$upd->position = 'Admin';
+		$upd->update();
+		return 'done';
+	}
+	public function setAdminToMember(Request $request) {
+		$upd = Group_member::where('group_id', '=', $request->group_id)->where('user_id', '=', $request->user_id)->where('position', '=', 'Admin')->get()->first();
+		$upd->position = 'Member';
+		$upd->update();
+		return 'done';
+	}
+	public function setToOwner(Request $request) {
+		$upd = Group_member::where('group_id', '=', $request->group_id)->where('user_id', '=', $request->user_id)->get()->first();
+		$upd->position = 'Owner';
+		$upd->update();
+		return 'done';
+	}
+
 }
